71a918ccf82d28ec4e9483b2f985db69bd04db83
Carl Andrew Castañas <89068725+carlcastanas@users.noreply.github.com>
2022-06-03 17:58:44 +0800

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fibonacci
-    </title>
+    <title>Fibonacci</title>
 </head>
 <body>
 <form name="fibonacci_form">
